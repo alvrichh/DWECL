4fd4a2d82d32e1123b1d8f2f964fc401d4bcdb74
alvrichh <alvarorodriguezmolina0@gmail.com>
2023-11-09 08:59:12 +0100

    Examen corregido

--- EXAMEN_RA3_ARM/examen03_ra3_arm.js
+++ EXAMEN_RA3_ARM/examen03_ra3_arm.js
@@ -1,10 +1,46 @@
-let palo = ('Oros, Copas, Espadas, Bastos');
-let valor;
-let numeros = [];
-maxPalos = 4;
-maxValor =10;
+function Carta(palo, valor) {
+    if (palo >= 1 && palo <= 4 && valor >= 1 && valor <= 10) {
+        this.palo = palo;
+        this.valor = valor;
+    } else {
+        console.log("No son valores válidos para una carta");
+    }
+    this.darValor = function (palo, valor) {
+        if (palo >= 1 && palo <= 4 && valor >= 1 && valor <= 10) {
+            this.palo = palo;
+            this.valor = valor;
+        } else {
+            console.log("No sirven los valores");
+        }
+    }
+    this.toString = function () {
+        const palos = ['Oros', 'Copas', 'Espadas', 'Bastos'];
+        return (this.valor + " de " + palos[this.palo - 1]);
+    }
+}
+function generarBaraja(){
+    const baraja = [];
+    for(let palo = 1; palo <=4; palo++){
+        for(valor = 1; valor <=10; valor++){
+            let nuevaCarta = new Carta(palo,valor);
+            baraja.push(nuevaCarta);
+        }
+    }
+    return baraja;
+}
+function mostrarBaraja(baraja) {
+    for (let i = 0; i < baraja.length; i++) {
+        console.log(baraja[i].toString());
+    }
+}
+const miBaraja = generarBaraja();
 
+mostrarBaraja(miBaraja);
 
+let miCarta = new Carta(1, 1);
+console.log(miCarta.palo);
+
+/*
 class Carta {
     constructor(palo, valor) {
         this.palo = palo;
@@ -41,4 +77,4 @@ function generarBaraja() {
     return baraja;
 }
 generarBaraja();
-
+*/
