67d835aa68f9c2bfc9a209c6756571e6b3eec333
Alvaro Rodriguez Molina <alvarorodriguezmolina0@gmail.com>
2023-11-14 19:48:40 +0100

    Cambios en el entregable

--- UNIDAD6/U6_ARM/Entregable1_ud6_ARM.js
+++ UNIDAD6/U6_ARM/Entregable1_ud6_ARM.js
@@ -1,14 +1,44 @@
+let html = document.getElementById("html");
+let bot = document.getElementById("bot");
+let js = document.getElementById("js");
+let centro = document.getElementById("centro");
 
-html =document.getElementById("html");
+let zoomInButton = document.getElementById("zoomIn");
+let zoomOutButton = document.getElementById("zoomOut");
+let tamanioActual = 100;
+let tamanioMaximo = 300;
+let tamanioMinimo = 5;
 
-let src = document.getAttribute('src');
-let imagen = document.querySelector("centro");
-
-
-function mostrarImagen(e){
-
-    html.src.imagen.setAttribute('img/html_logo.png')
-
+function mostrarImagen(e) {
+    let src = e.target.getAttribute("src");
+    centro.setAttribute("src", src);
+    tamanioActual = 100;
+    aplicarTamanio();
 }
 
-html.addEventListener("click", mostrarImagen)
+function zoomIn() {
+    tamanioActual += 30;
+    if (tamanioActual > tamanioMaximo) {
+        tamanioActual = tamanioMaximo;
+    }
+    aplicarTamanio();
+}
+
+function zoomOut() {
+    tamanioActual -= 30;
+    if (tamanioActual < tamanioMinimo) {
+        tamanioActual = tamanioMinimo;
+    }
+    aplicarTamanio();
+}
+
+function aplicarTamanio() {
+    centro.style.width = tamanioActual + "px";
+    centro.style.height = tamanioActual + "px";
+}
+
+html.addEventListener("click", mostrarImagen);
+bot.addEventListener("click", mostrarImagen);
+js.addEventListener("click", mostrarImagen);
+zoomInButton.addEventListener("click", zoomIn);
+zoomOutButton.addEventListener("click", zoomOut);
