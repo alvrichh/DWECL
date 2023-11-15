e520f68b317852f0cdefe05626c1a3a5ff82d8ba
alvrichh <alvarorodriguezmolina0@gmail.com>
2023-11-15 14:42:09 +0100

    no funciona los colores cambios en css

--- UNIDAD6/U6_ARM/Entregable1_ud6_ARM.js
+++ UNIDAD6/U6_ARM/Entregable1_ud6_ARM.js
@@ -6,18 +6,20 @@ let centro = document.getElementById("centro");
 let zoomInButton = document.getElementById("zoomIn");
 let zoomOutButton = document.getElementById("zoomOut");
 let tamanioActual = 100;
-let tamanioMaximo = 300;
+let tamanioMaximo = 500;
 let tamanioMinimo = 5;
 //----------------COLORES--------------
+let cuadrados = document.querySelectorAll('cuadrado');
+
 
 function mostrarImagen(e) {
     let src = e.target.getAttribute("src"); //el target devuelve el nombre del elemento que roduce el elemento
     centro.setAttribute("src", src);
-    tamanioActual = 100;
+    //tamanioActual = 100; //para que se resetee el tamaño de la imagen
     aplicarTamanio();
 }
 
-function zoomIn() {
+function zoomIn(e) {
     tamanioActual += 30;
     if (tamanioActual > tamanioMaximo) {
         tamanioActual = tamanioMaximo;
@@ -25,7 +27,7 @@ function zoomIn() {
     aplicarTamanio();
 }
 
-function zoomOut() {
+function zoomOut(e) {
     tamanioActual -= 30;
     if (tamanioActual < tamanioMinimo) {
         tamanioActual = tamanioMinimo;
@@ -33,9 +35,14 @@ function zoomOut() {
     aplicarTamanio();
 }
 
-function aplicarTamanio() {
+function aplicarTamanio(e) {
     centro.style.width = tamanioActual + "px";
     centro.style.height = tamanioActual + "px";
+}
+
+function cambiarColor(e){
+    centro.style.border = color;
+    
 }
 
 html.addEventListener("click", mostrarImagen);
