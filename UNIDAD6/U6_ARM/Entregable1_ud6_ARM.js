38e30adbc808320b48caceccfa68d4b6a16971f1
alvrichh <alvarorodriguezmolina0@gmail.com>
2023-11-16 10:37:29 +0100

    Ejercicio terminado

--- UNIDAD6/U6_ARM/Entregable1_ud6_ARM.js
+++ UNIDAD6/U6_ARM/Entregable1_ud6_ARM.js
@@ -1,7 +1,14 @@
 let html = document.getElementById("html");
 let bot = document.getElementById("bot");
 let js = document.getElementById("js");
+
+//imagen del centro con id="centro"
 let centro = document.getElementById("centro");
+
+//Lo utilizo para los colores
+let frame = document.querySelector('.frame');
+
+
 //---------------ZOOM--------------
 let zoomInButton = document.getElementById("zoomIn");
 let zoomOutButton = document.getElementById("zoomOut");
@@ -9,11 +16,12 @@ let tamanioActual = 100;
 let tamanioMaximo = 500;
 let tamanioMinimo = 5;
 //----------------COLORES--------------
-let cuadrados = document.querySelectorAll('cuadrado');
+let colores = document.querySelectorAll('.colores');
+
 
 
 function mostrarImagen(e) {
-    let src = e.target.getAttribute("src"); //el target devuelve el nombre del elemento que roduce el elemento
+    let src = e.target.getAttribute("src"); //el target devuelve el nombre del elemento que recibe el elemento
     centro.setAttribute("src", src);
     //tamanioActual = 100; //para que se resetee el tamaño de la imagen
     aplicarTamanio();
@@ -36,13 +44,37 @@ function zoomOut(e) {
 }
 
 function aplicarTamanio(e) {
+    // "+ 'o' -" 30px
     centro.style.width = tamanioActual + "px";
     centro.style.height = tamanioActual + "px";
 }
 
-function cambiarColor(e){
-    centro.style.border = color;
-    
+
+function cambiarColor(e) {
+    let colorId = e.target.id;
+    switch (colorId) {
+        case 'black':
+            frame.style.borderColor = 'black';
+            break;
+        case 'red':
+            frame.style.borderColor = 'red';
+            break;
+        case 'blue':
+            frame.style.borderColor = 'blue';
+            break;
+        case 'yellow':
+            frame.style.borderColor = 'yellow';
+            break;
+        case 'green':
+            frame.style.borderColor = 'green';
+            break;
+        case 'pink':
+            frame.style.borderColor = 'pink';
+            break;
+        default:
+            frame.style.borderColor = 'black'; // Cambiar a color por defecto si es necesario
+            break;
+    }
 }
 
 html.addEventListener("click", mostrarImagen);
@@ -50,3 +82,8 @@ bot.addEventListener("click", mostrarImagen);
 js.addEventListener("click", mostrarImagen);
 zoomInButton.addEventListener("click", zoomIn);
 zoomOutButton.addEventListener("click", zoomOut);
+
+// Es necesario el for para que recorra el array del querySelectorAll
+for (let cuadrado of colores) {
+    cuadrado.addEventListener('mouseover', cambiarColor);
+}
