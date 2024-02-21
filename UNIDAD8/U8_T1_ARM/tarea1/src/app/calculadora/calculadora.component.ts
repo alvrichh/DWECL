9f360691a16968533b40ef0e69a8e5468a692e34
Alvaro Rodriguez Molina <alvarorodriguezmolina0@gmail.com>
2024-02-21 20:56:35 +0100

    un poco de leña por aquí

--- UNIDAD8/U8_T1_ARM/tarea1/src/app/calculadora/calculadora.component.ts
+++ UNIDAD8/U8_T1_ARM/tarea1/src/app/calculadora/calculadora.component.ts
@@ -1,22 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule , MatCardModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.css'
+  styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
 
-  num1:number=0;
-  num2:number=0;
-  res:number= 0;
-  suma(){
-    this.res = this.num1+this.num2;
-    this.num1 = 0;
-    this.num2 = 0;
+  num1: number = 0;
+  num2: number = 0;
+  res: number = 0;
+
+  suma() {
+    this.res = this.num1 + this.num2;
+  }
+
+  resta() {
+    this.res = this.num1 - this.num2;
+  }
+
+  multiplica() {
+    this.res = this.num1 * this.num2;
   }
 }
--- UNIDAD8/U8_T1_ARM/tarea2/src/app/app.component.ts
+++ UNIDAD8/U8_T1_ARM/tarea2/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalculadoraComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tarea1';
+  title = 'tarea2';
 }
