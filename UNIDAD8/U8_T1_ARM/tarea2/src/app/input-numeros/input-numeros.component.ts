610b07396d5446079f25200d15631b5a0d9ecbbd
alvrichh <alvarorodriguezmolina0@gmail.com>
2024-02-22 09:52:53 +0100

    tarea2 completada

--- UNIDAD8/U8_T1_ARM/tarea2/src/app/input-numeros/input-numeros.component.ts
+++ UNIDAD8/U8_T1_ARM/tarea2/src/app/input-numeros/input-numeros.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { OperacionesComponent } from "../operaciones/operaciones.component";
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-input-numeros',
-  standalone: true,
-  imports: [FormsModule, CommonModule],
-  templateUrl: './input-numeros.component.html',
-  styleUrl: './input-numeros.component.css'
+    selector: 'app-input-numeros',
+    standalone: true,
+    templateUrl: './input-numeros.component.html',
+    styleUrl: './input-numeros.component.css',
+    imports: [FormsModule, CommonModule, OperacionesComponent, MatCardModule]
 })
 export class InputNumerosComponent {
   num1: number = 0;
   num2: number = 0;
+resultado: any;
 }
--- UNIDAD8/U8_T1_ARM/tarea2/src/app/operaciones/operaciones.component.ts
+++ UNIDAD8/U8_T1_ARM/tarea2/src/app/operaciones/operaciones.component.ts
@@ -2,29 +2,35 @@ import { Component } from '@angular/core';
 import { InputNumerosComponent } from '../input-numeros/input-numeros.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Input} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-operaciones',
   standalone: true,
-  imports: [FormsModule, CommonModule, InputNumerosComponent],
+  imports: [FormsModule, CommonModule, InputNumerosComponent, MatButtonModule],
   templateUrl: './operaciones.component.html',
   styleUrl: './operaciones.component.css'
 })
 export class OperacionesComponent {
   resultado: number = 0;
+  @Input()numero1:InputNumerosComponent | any;
+  @Input()numero2:InputNumerosComponent | any;
 
 
-  constructor(private inputNumeros: InputNumerosComponent) {}
+  //constructor(private inputNumeros: InputNumerosComponent) {}
 
   sumar() {
-    this.resultado = this.inputNumeros.num1 + this.inputNumeros.num2;
+   this.resultado = this.numero1 + this.numero2;
+
   }
 
   restar() {
-    this.resultado = this.inputNumeros.num1 - this.inputNumeros.num2;
+    this.resultado = this.numero1 - this.numero2;
+    
   }
 
   multiplicar() {
-    this.resultado = this.inputNumeros.num1 * this.inputNumeros.num2;
+    this.resultado = this.numero1 * this.numero2;
   }
 }
